21ae3ee8bee907220e5c68a521fab5b988cda2b3
Ben Gubler <nebrelbug@gmail.com>
2020-09-11 17:39:15 -0600

    Update Docusaurus

--- sidebars.js
+++ sidebars.js
@@ -1,53 +1,53 @@
 module.exports = {
   docs: {
-    'Get Started': [
-      'get-started/overview',
-      'get-started/install',
-      'get-started/security',
-      'get-started/first-template'
+    "Get Started": [
+      "get-started/overview",
+      "get-started/install",
+      "get-started/security",
+      "get-started/first-template"
     ],
-    'Template Syntax': [
-      'syntax/overview',
-      'syntax/cheatsheet',
-      'syntax/async',
-      'syntax/interpolate',
-      'syntax/auto-escaping',
-      'syntax/evaluate',
-      'syntax/partials',
-      'syntax/whitespace-control'
+    "Template Syntax": [
+      "syntax/overview",
+      "syntax/cheatsheet",
+      "syntax/async",
+      "syntax/interpolate",
+      "syntax/auto-escaping",
+      "syntax/evaluate",
+      "syntax/partials",
+      "syntax/whitespace-control"
     ],
     API: [
-      'api/overview',
-      'api/rendering',
-      'api/compilation',
-      'api/configuration',
-      'api/file-handling',
-      'api/parsing',
-      'api/templates-partials'
+      "api/overview",
+      "api/rendering",
+      "api/compilation",
+      "api/configuration",
+      "api/file-handling",
+      "api/parsing",
+      "api/templates-partials"
     ]
 
     // add custom tags, how-to caching, etc.
   },
   about: [
-    'about/introduction',
-    'about/eta-vs-ejs',
-    'about/why-eta',
-    'about/performance',
-    'about/FAQ',
-    'about/how-eta-works'
+    "about/introduction",
+    "about/eta-vs-ejs",
+    "about/why-eta",
+    "about/performance",
+    "about/FAQ",
+    "about/how-eta-works"
   ],
 
   // add custom tags, how-to caching, etc.
-  Learn: [
+  learn: [
     {
-      type: 'category',
-      label: 'Learn',
-      items: ['learn/async', 'learn/interactive'] // TODO: add tutorials on plugins, etc.
+      type: "category",
+      label: "Learn",
+      items: ["learn/async", "learn/interactive"] // TODO: add tutorials on plugins, etc.
     },
     {
-      type: 'category',
-      label: 'Resources',
-      items: ['resources/integrations']
+      type: "category",
+      label: "Resources",
+      items: ["resources/integrations"]
     }
   ]
 
