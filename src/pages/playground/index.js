7248a1bf4c94dcbed93c9f4d32c3dac662b17508
Ben Gubler <nebrelbug@gmail.com>
2023-01-30 22:11:20 -0700

    Update imports

--- src/pages/playground/index.js
+++ src/pages/playground/index.js
@@ -2,9 +2,7 @@ import React from "react"
 import Layout from "@theme/Layout"
 import styles from "./index.module.css"
 import datStyles from "./dat-gui.module.css"
-import CodeBlock from "@theme/CodeBlock"
-var Eta = require("eta")
-// import * as Eta from "eta/browser.js"
+import * as Eta from "eta"
 import DatGui, { DatBoolean, DatSelect, DatString } from "react-dat-gui"
 
 import "./react-dat-gui.css"
