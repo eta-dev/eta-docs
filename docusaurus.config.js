e47229ee43eebd8306dac2bdba912847224a4eb0
Ben Gubler <nebrelbug@gmail.com>
2023-02-07 16:29:59 -0700

    GA4 migration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,6 +46,10 @@ const config = {
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css")
+        },
+        gtag: {
+          trackingID: "G-RZPZ730L0H",
+          anonymizeIP: true
         }
       })
     ]
